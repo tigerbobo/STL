b1067f9930b0a5489501c32405c3415179059839
tigerbobo <tigerwd890@163.com>
2019-10-22 13:49:26 +0800

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -1,18 +1,20 @@
+#include <map>
+
 void example(void)
+{
+	int id;
+	std::string string;
+	std::map<int, std::string> map;
+	std::map<int, std::string>::iterator iterator;
+
+	iterator = map.begin();
+	if (iterator == map.end())
 	{
-		int id;
-		std::string string;
-		std::map<int, std::string> map;
-		std::map<int, std::string>::iterator iterator;
-
-		iterator = map.begin();
-		if (iterator == map.end())
-		{
-			map[5] = "5555";
-			map[6] = "6666";
-			map[7] = "7777";
-		}
-
-		id = iterator->first;
-		string = iterator->second;
+		map[5] = "5555";
+		map[6] = "6666";
+		map[7] = "7777";
 	}
+
+	id = iterator->first;
+	string = iterator->second;
+}
